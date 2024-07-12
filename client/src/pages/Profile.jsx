ce84e75ec20bb975da49f723b731917230606c83
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-07-12 12:00:13 +0500

    complete profile ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,10 +8,15 @@ const Profile = () => {
       <h1 className='text-3xl font-semibold text-center my-6'>Profile</h1>
       <form className='flex w-full flex-col items-center justify-center gap-4'>
         <img className="w-20 h-20 rounded-full" src={currentUser.avatar} alt=""/>
-        <input type='text' placeholder='email' className='border p-3 rounded-lg' id='email'/>
-        <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'/>
+        <input type='text' placeholder='username' className='w-[50%] border p-3 rounded-lg' id='username'/>
+        <input type='text' placeholder='email' className='w-[50%] border p-3 rounded-lg' id='email'/>
+        <input type='password' placeholder='password' className='w-[50%] border p-3 rounded-lg' id='password'/>
         <button className='w-[50%] bg-slate-700 text-white p-3 rounded-lg uppercase'>Update</button>
       </form>
+      <div className='flex justify-between px-52 py-2 text-red-600'>
+        <span>Delete Account</span>
+        <span>Sign Out</span>
+      </div>
     </div>
   )
 }
