321246bf59cf2096e480f9951995061e0844524a
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-07-22 21:26:18 +0500

    viteconfigggggg

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:3000",
+        target: "https://feelhome.onrender.com" || "http://localhost:3000",
         secure: false,
+        changeOrigin: true,
       },
     },
   },
-  plugins: [react()]
+  plugins: [react()],
 });
