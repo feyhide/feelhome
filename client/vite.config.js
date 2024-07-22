c752a4a5dd06b40e343117ef837f4cdb5eb39a53
MUHAMMAD FAHAD <141734135+fahad-0900@users.noreply.github.com>
2024-07-22 11:49:15 +0500

    vite config2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api' : {
-        target:"http://localhost:3000",
-        secure:false,
+  server: {
+    proxy: {
+      '/api': {
+        target: "http://localhost:3000",
+        secure: false,
       },
     },
   },
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-redux'],
-    }
-  }
-})
+      external: ['react-redux', 'redux-persist'],
+    },
+  },
+});
